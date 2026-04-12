059dc1c8579581835a85bcaccd62700ad6d47e71
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-04-12 16:37:52 -0500

    Update a task 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module that returns a list of school having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    Args:
+        mongo_collection: pymongo collection object
+        topic: (string) topic searched
+    Returns:
+        List of schools
+    """
+    return list(mongo_collection.find({"topics": topic}))
