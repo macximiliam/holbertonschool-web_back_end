1975ce3cf4202fb320923cdcccc2c6459f0cece9
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-04-22 17:18:53 -0500

    answer task 2

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,7 +1,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  // Usamos una arrow function para mantener el contexto de 'this' automáticamente
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
