50103d367da6b35be2dd2de92e55cc39f8f5b75a
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-04-24 16:37:40 -0500

    answer task 0

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  /**
+   * Representa un salón de clases.
+   * @param {Number} maxStudentsSize
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
