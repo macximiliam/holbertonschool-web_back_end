022e84429019aeceac2b705b25a0b7e4fe5dfd8c
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-01 18:51:47 -0500

    function getStudentIdsSum that returns the sum of all the student ids.

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the sum of all student IDs.
+ * @param {Array} students - The list of student objects.
+ * @returns {Number} The total sum of all student IDs.
+ */
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+
+  return students.reduce((accumulator, student) => accumulator + student.id, 0);
+}
