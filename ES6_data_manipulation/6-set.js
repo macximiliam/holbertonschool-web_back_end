b71ea7c2fc6eec6f646e45b979c196ffcdd532fd
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-03 15:57:50 -0500

    function named setFromArray that returns a Set from an array

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a Set from an array.
+ * @param {Array} array - The array to convert into a Set.
+ * @returns {Set} A Set containing the unique elements of the array.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
