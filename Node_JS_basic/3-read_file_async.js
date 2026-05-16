b81e74652bf6673d25dca943b9c31ee7baa335d9
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-16 02:10:40 -0500

    Add 3-read_file_async.js solution

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,23 +1,20 @@
 const fs = require('fs');
 
 function countStudents(path) {
-
   return new Promise((resolve, reject) => {
-
     fs.readFile(path, 'utf-8', (error, data) => {
-      
       if (error) {
-
         reject(new Error('Cannot load the database'));
         return;
       }
 
       if (data) {
         const lines = data.trim().split('\n').filter((line) => line.length > 0);
-        
+
         console.log(`Number of students: ${lines.length - 1}`);
 
         const fields = {};
+
         for (let i = 1; i < lines.length; i += 1) {
           const student = lines[i].split(',');
           const firstName = student[0];
@@ -32,7 +29,6 @@ function countStudents(path) {
         for (const field in fields) {
           if (Object.prototype.hasOwnProperty.call(fields, field)) {
             const list = fields[field];
-
             console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
           }
         }
