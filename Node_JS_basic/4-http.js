2d6758a140284a4936a32f52210ab5334efb0358
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-16 15:47:45 -0500

    Add 4-http.js solution

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const HOST = '1245'; 
+const PORT = 1245;
+
+
+const app = http.createServer((req, res) => {
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.write('Hello Holberton School!');
+
+  res.end();
+});
+
+app.listen(PORT, () => {
+
+});
+
+module.exports = app;
