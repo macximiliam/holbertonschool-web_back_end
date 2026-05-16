7eaedf040da5861023d064dd7de3b0e766157ba3
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-15 23:39:07 -0500

    Interactive script that reads user input from STDIN

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,20 +4,22 @@
  * Finally, it displays a closing message when the stream ends.
  */
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
-// 1. Listen for the event to start reading data
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
 
   if (input !== null) {
-    // 2. Output the captured name
-    process.stdout.write('Your name is: ' + input.trim() + '\n');
+
+    process.stdout.write(`Your name is: ${input.trim()}\n`);
   }
 });
 
-// 3. Listen for the event when the input stream is finished
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
