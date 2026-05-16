4de6ba9dcb5501c377fd0d6e8123ce920e076e1d
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-16 01:25:44 -0500

    Interactive script that reads user input from STDIN

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (input) => {
-  if (input !== null && input !== '\n') {
-    process.stdout.write(`Your name is: ${input.replace(/\n$/, '')}\n`);
-  }
+
+process.stdin.setEncoding('utf8');
+
+// Handle user input
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
+// Handle program termination
 process.stdin.on('end', () => {
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-  }
+  process.stdout.write('This important software is now closing\n');
 });
