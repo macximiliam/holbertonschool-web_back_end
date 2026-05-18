e91c1f990cc3a8e116fea3aa9916c50bfdb37b51
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-17 19:54:38 -0500

    Fix requirements and export definitions for task 8

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,17 +3,17 @@ import readDatabase from '../utils';
 export default class StudentsController {
   static getAllStudents(request, response) {
     const databaseFile = process.argv[2] || '';
-    
-    readDatabase(databaseFile)
+
+    return readDatabase(databaseFile)
       .then((students) => {
-        let output = 'This is the list of our students';
+        const output = ['This is the list of our students'];
         const fields = Object.keys(students).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-        
+
         fields.forEach((field) => {
-          output += `\nNumber of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`;
+          output.push(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
         });
-        
-        return response.status(200).send(output);
+
+        return response.status(200).send(output.join('\n'));
       })
       .catch(() => response.status(500).send('Cannot load the database'));
   }
@@ -21,11 +21,11 @@ export default class StudentsController {
   static getAllStudentsByMajor(request, response) {
     const databaseFile = process.argv[2] || '';
     const { major } = request.params;
-    
+
     if (major !== 'CS' && major !== 'SWE') {
       return response.status(500).send('Major parameter must be CS or SWE');
     }
-    
+
     return readDatabase(databaseFile)
       .then((students) => {
         const list = students[major] ? students[major].join(', ') : '';
