f96a74522cdce09e1f67c11ab94f73996091635e
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-17 19:19:14 -0500

    Add task 8 solution

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,36 @@
+import { readDatabase } from '../utils';
+
+export default class StudentsController {
+  static getAllStudents(request, response) {
+    const databaseFile = process.argv[2];
+    
+    readDatabase(databaseFile)
+      .then((students) => {
+        let output = 'This is the list of our students';
+        const fields = Object.keys(students).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        
+        fields.forEach((field) => {
+          output += `\nNumber of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`;
+        });
+        
+        return response.status(200).send(output);
+      })
+      .catch(() => response.status(500).send('Cannot load the database'));
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const databaseFile = process.argv[2];
+    const { major } = request.params;
+    
+    if (major !== 'CS' && major !== 'SWE') {
+      return response.status(500).send('Major parameter must be CS or SWE');
+    }
+    
+    return readDatabase(databaseFile)
+      .then((students) => {
+        const list = students[major] ? students[major].join(', ') : '';
+        return response.status(200).send(`List: ${list}`);
+      })
+      .catch(() => response.status(500).send('Cannot load the database'));
+  }
+}
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const lines = data.split('\n');
+      const students = {};
+      
+      for (let i = 1; i < lines.length; i += 1) {
+        if (lines[i].trim() !== '') {
+          const row = lines[i].split(',');
+          const firstName = row[0];
+          const field = row[3];
+          
+          if (firstName && field) {
+            if (!students[field]) {
+              students[field] = [];
+            }
+            students[field].push(firstName);
+          }
+        }
+      }
+      resolve(students);
+    });
+  });
+}
